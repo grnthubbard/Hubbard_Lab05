fc373f5d6dec0e11650a04831b889e4950f31516
Grant D Hubbard <gdhubbar@purdue.edu>
2022-09-21 19:33:58 -0400

    1st try

--- Hubbard_Lab05/Hubbard_Lab05.cpp
+++ Hubbard_Lab05/Hubbard_Lab05.cpp
@@ -5,12 +5,42 @@
 #include <vector>
 #include <string>
 
-
+using namespace std;
 
 int main()
 {
 
+	vector<char> stringCypher { 'V','F','X','B','L','I','T','Z','J','R','P','H','D','K','N','O','W','S','G','U','Y','Q','M','A','C','E' };
+	string test;
+	do {
 
+		cout << "Enter a phrase you want to be cyphered!";
+		getline(cin, test);
+		int stringLength = 0;
+		int stringLength = test.length();
+
+		for (int i = 0; i < stringLength; i++) {
+			if (test[i] >= 'A' && test[i] <= 'Z') //Is a capital letter //
+			{
+				stringCypher;
+			}
+			else if (test[i] >=97 & <= 122) {
+				test[i] == char[-32]
+			
+			
+			
+			}
+
+
+
+
+			}
+		}
+		
+
+
+
+	} while 
 
 
 
